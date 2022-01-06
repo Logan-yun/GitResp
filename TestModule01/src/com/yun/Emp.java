e1e434076ce56f8d548e6b09a680d04f13fe4aa8
yuncheng <1337028640@qq.com>
2022-01-06 22:18:40 +0800

    增加Emp实体类

---
+++ TestModule01/src/com/yun/Emp.java
@@ -0,0 +1,14 @@
+package com.yun;
+
+import java.io.Serializable;
+
+/**
+ * @author yunCheng
+ * @version: 1.0
+ */
+public class Emp implements Serializable {
+    private String name;
+    private Integer age;
+    private Double height;
+    private Double weight;
+}
